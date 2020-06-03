4e036bc7518cb34958bff341773c7f3ad64084d3
Bojo <bozhidar_zahariev@hotmail.com>
2020-06-03 14:18:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let question1Text = 'Question1?';
+let answer1a = 'answer1a';
+let answer1b = 'answer1b';
+let answer1c = 'answer1c';
+let answer1d = 'answer1d';
+
+let question1answer = 'answer1a';
+
+const question = document.querySelector('#question');
+question.textContent = question1Text;
