3bb1d5d113249002943e7b60793a753036d33807
Bojo <bozhidar_zahariev@hotmail.com>
2020-06-04 11:24:14 +0100

    for each

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+/*CHANGE BELOW*/
 let question1Text = 'Question1?';
 let answer1a = 'answer1a';
 let answer1b = 'answer1b';
 let answer1c = 'answer1c';
 let answer1d = 'answer1d';
 
-let question1answer = 'answer1a';
+let question1correct = 'answer1a';
+/*CHANGE ABOVE*/
 
+const answers = document.querySelectorAll('.answers');
 const question = document.querySelector('#question');
 question.textContent = question1Text;
+
+Array.from(answers).forEach(element => {
+  element.addEventListener('click', e => {
+    console.log(element);
+  });
+});
