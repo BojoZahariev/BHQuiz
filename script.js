afa8d31470f23439d479fa88417b93a46e8172c7
Bojo <bozhidar_zahariev@hotmail.com>
2020-06-09 15:15:31 +0100

    prize

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var score = [];
 var allWrongs = [];
 var choice;
+var maxScore = 3;
 var answered1 = false;
 var answered2 = false;
 var answered3 = false;
@@ -26,6 +27,7 @@ const final = document.querySelector('#final');
 const finalScore = document.querySelector('#finalScore');
 const reload = document.querySelector('#reload');
 const wrongAnswers = document.querySelector('#wrongAnswers');
+const prizeImg = document.querySelector('#prizeImg');
 
 //const answers2 = document.querySelectorAll('.answers2');
 
@@ -104,6 +106,18 @@ const checkClicked = cl => {
   return Array.from(allAnswers).some(hasIt);
 };
 
+const sumResult = () => {
+  //summarize all score
+  const add = (a, b) => a + b;
+  const sum = score.reduce(add);
+  return sum;
+};
+
+const getPrize = () => {
+  prizeImg.style.display = 'block';
+  confetti();
+};
+
 startBtn.addEventListener('click', e => {
   headDiv.style.display = 'none';
   quiz1.style.display = 'flex';
@@ -149,11 +163,11 @@ finish.addEventListener('click', e => {
       }
     });
 
-    //summarize all score
-    const add = (a, b) => a + b;
-    const sum = score.reduce(add);
-    final.style.display = 'block';
-    finalScore.textContent = sum;
+    finalScore.textContent = `${sumResult()} / 3`;
+    if (sumResult() === maxScore) {
+      getPrize();
+    }
+    final.style.display = 'flex';
   } else {
     mes3.style.display = 'block';
   }
@@ -168,3 +182,108 @@ pre3.addEventListener('click', e => {
 reload.addEventListener('click', e => {
   window.location.reload();
 });
+
+const confetti = () => {
+  let W = window.innerWidth;
+  let H = window.innerHeight;
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+  const maxConfettis = 150;
+  const particles = [];
+
+  const possibleColors = [
+    'DodgerBlue',
+    'OliveDrab',
+    'Gold',
+    'Pink',
+    'SlateBlue',
+    'LightBlue',
+    'Gold',
+    'Violet',
+    'PaleGreen',
+    'SteelBlue',
+    'SandyBrown',
+    'Chocolate',
+    'Crimson'
+  ];
+
+  function randomFromTo(from, to) {
+    return Math.floor(Math.random() * (to - from + 1) + from);
+  }
+
+  function confettiParticle() {
+    this.x = Math.random() * W; // x
+    this.y = Math.random() * H - H; // y
+    this.r = randomFromTo(11, 33); // radius
+    this.d = Math.random() * maxConfettis + 11;
+    this.color = possibleColors[Math.floor(Math.random() * possibleColors.length)];
+    this.tilt = Math.floor(Math.random() * 33) - 11;
+    this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
+    this.tiltAngle = 0;
+
+    this.draw = function() {
+      context.beginPath();
+      context.lineWidth = this.r / 2;
+      context.strokeStyle = this.color;
+      context.moveTo(this.x + this.tilt + this.r / 3, this.y);
+      context.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5);
+      return context.stroke();
+    };
+  }
+
+  function Draw() {
+    const results = [];
+
+    // Magical recursive functional love
+    requestAnimationFrame(Draw);
+
+    context.clearRect(0, 0, W, window.innerHeight);
+
+    for (var i = 0; i < maxConfettis; i++) {
+      results.push(particles[i].draw());
+    }
+
+    let particle = {};
+    let remainingFlakes = 0;
+    for (var i = 0; i < maxConfettis; i++) {
+      particle = particles[i];
+
+      particle.tiltAngle += particle.tiltAngleIncremental;
+      particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
+      particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
+
+      if (particle.y <= H) remainingFlakes++;
+
+      // If a confetti has fluttered out of view,
+      // bring it back to above the viewport and let if re-fall.
+      if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
+        particle.x = Math.random() * W;
+        particle.y = -30;
+        particle.tilt = Math.floor(Math.random() * 10) - 20;
+      }
+    }
+
+    return results;
+  }
+
+  window.addEventListener(
+    'resize',
+    function() {
+      W = window.innerWidth;
+      H = window.innerHeight;
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    },
+    false
+  );
+
+  // Push new confetti objects to `particles[]`
+  for (var i = 0; i < maxConfettis; i++) {
+    particles.push(new confettiParticle());
+  }
+
+  // Initialize
+  canvas.width = W;
+  canvas.height = H;
+  Draw();
+};
